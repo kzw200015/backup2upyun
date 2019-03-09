ef7e8b89100f7ffffc5374655f133e41ed1a0914
kzw200015 <kzw200015@gmail.com>
2019-03-09 17:50:43 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,45 @@
+## 七牛云备份脚本
+
+自动上传网站文件到七牛云，并删除三天前的旧备份
+
+用python实现，需要依赖库`upyun`，以及用来打包的`zip`
+
+## 食用方法
+
+直接使用git clone整个库
+
+`git clone https://github.com/kzw200015/backup-qiniu.git`
+
+安装 python 环境
+
+`apt install -y python3`
+
+进入脚本目录
+
+`cd /root/backup2upyun`
+
+安装依赖
+
+`apt install -y python3-pip`
+
+`pip3 install upyun`
+
+把配置文件复制一份
+
+`cp config.py.example config.py`
+
+按照`config.py`内的说明进行配置
+
+执行脚本
+
+`./main.py`
+
+## 定时任务
+用`cron`实现
+执行`crontab -e`，加入以下内容
+```
+LANG='en_US.UTF-8'
+LC_ALL='en_US.UTF-8'
+0 2 * * * /root/backup2upyun/main.py
+```
+保存退出即可，这样每天凌晨两点就会自动执行一次任务
---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import upyun
+import os
+from datetime import datetime, timedelta
+from config import *
+
+
+class backup2upyun(object):
+
+    def __init__(self, service_name, operator_user, operator_passwd, backup_mark, backup_dir, backup_pre_dir, backup_database, mysql_host, mysql_user, mysql_passwd):
+        self.up = upyun.UpYun(service_name, operator_user, operator_passwd)
+        self.backup_mark = backup_mark
+        self.backup_dir = backup_dir
+        self.backup_pre_dir = backup_pre_dir
+        self.backup_database = backup_database
+        self.mysql_host = mysql_host
+        self.mysql_user = mysql_user
+        self.mysql_passwd = mysql_passwd
+
+    def __start(self):
+        self.cwd = os.getcwd()
+        self.old_key = self.backup_mark + '-' + \
+            (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d') + '.zip'
+        self.current_key = self.backup_mark + '-' + \
+            (datetime.now()).strftime('%Y-%m-%d') + '.zip'
+        self.local_file_path = os.path.join(
+            self.backup_pre_dir, self.current_key)
+        os.chdir(self.backup_pre_dir)
+
+    def __tarFiles(self):
+        for each_dir in self.backup_dir:
+            os.system(' '.join(['zip -q -r', each_dir + '.zip', each_dir]))
+
+    def __dumpMysql(self):
+        for each_database in self.backup_database:
+            os.system(' '.join(['mysqldump -h', self.mysql_host, '-u' + self.mysql_user,
+                                '-p' + self.mysql_passwd, each_database, '>', each_database + '.sql']))
+
+    def __uploadFile(self):
+        os.system(' '.join(['zip -q -r', self.current_key, '*.zip *.sql']))
+        with open(self.local_file_path, 'rb') as f:
+            self.up.put(self.current_key, f, checksum=True)
+
+    def __end(self):
+        self.up.delete(self.old_key)
+        os.system('rm -rf *.zip *.sql')
+        os.chdir(self.cwd)
+
+    def do(self):
+        self.__start()
+        print('正在打包文件')
+        self.__tarFiles()
+        self.__dumpMysql()
+        print('正在上传')
+        self.__uploadFile()
+        print('正在删除临时文件以及旧备份')
+        self.__end()
+
+
+if __name__ == '__main__':
+    bak = backup2upyun(service_name, operator_user, operator_passwd, backup_mark,
+                       backup_dir, backup_pre_dir, backup_database, mysql_host, mysql_user, mysql_passwd)
+    bak.do()
