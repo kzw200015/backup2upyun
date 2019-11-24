91750c1ce0b01c64bc304d1c50c340351ad7380f
kzw200015 <kzw200015@gmail.com>
2019-11-24 18:23:09 +0800

    修复空目录不被打包的bug，现在上传时将会显示进度条

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from datetime import datetime, timedelta
 from config import *
 from progressbar import Percentage, FileTransferSpeed, Bar, ETA, ProgressBar
 import upyun
-import zipfile
+import tarfile
 import os
+import shutil
 
 
 class ProgressBarHandler(object):
@@ -21,25 +22,23 @@ class ProgressBarHandler(object):
         self.pbar.finish()
 
 
-current_backup = backup_mark + '-' + (datetime.now()).strftime('%Y-%m-%d') + '.zip'
-outdated_backup = backup_mark + '-' + (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d') + '.zip'
+current_backup = backup_mark + '-' + (datetime.now()).strftime('%Y-%m-%d') + '.tar.gz'
+outdated_backup = backup_mark + '-' + (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d') + '.tar.gz'
 
-
-with zipfile.ZipFile(current_backup, 'w') as zf:
+with tarfile.open(current_backup, 'w:gz') as tf:
 
     for each_database in backup_database:
-        print('dumping database[{}]...'.format(each_database))
+        print('dumping database [{}]...'.format(each_database))
         os.system('mysqldump -h{} --default-character-set={}  -u{} -p{} {} > {}.sql'.format(mysql_host, mysql_charset[each_database], mysql_user, mysql_passwd, each_database, each_database))
-        print('taring sql file[{}]...'.format(each_database + '.sql'))
-        zf.write(each_database + '.sql', compress_type=zipfile.ZIP_DEFLATED)
-        print('cleaning temp sql file[{}]...'.format(each_database + '.sql'))
+        print('taring sql file [{}]...'.format(each_database + '.sql'))
+        tf.add(each_database + '.sql')
         os.remove(each_database + '.sql')
 
     for each_dir in backup_dir:
-        print('taring dir[{}]'.format(each_dir))
-        for root, dirs, files in os.walk(os.path.join(backup_pre_dir, each_dir)):
-            for name in files:
-                zf.write(os.path.join(root, name), os.path.join(root, name).replace(backup_pre_dir, ''), compress_type=zipfile.ZIP_DEFLATED)
+        print('taring dir [{}]'.format(each_dir))
+        tarFileName = shutil.make_archive(each_dir, 'gztar', root_dir=backup_pre_dir, base_dir=each_dir)
+        tf.add(tarFileName, os.path.basename(tarFileName))
+        os.remove(tarFileName)
 
 up = upyun.UpYun(service_name, operator_user, operator_passwd)
 
@@ -47,12 +46,14 @@ with open(current_backup, 'rb') as f:
     res = up.put(current_backup, f, checksum=True, handler=ProgressBarHandler, params="uploading ")
 
 print('deleting outdated backup...')
+
 try:
     up.delete(outdated_backup)
 except Exception as e:
     print('delete failed' + '\n' + str(e))
 
 print('cleaning temp files...')
+
 os.remove(current_backup)
 
 print('done!')
